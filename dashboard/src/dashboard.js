1ca45aaf9dcffc70f66f93dd661c76416b2003e1
JoseXS <xskunk@gmail.com>
2023-05-29 09:46:06 +0200

    first commit

---
+++ dashboard/src/dashboard.js
@@ -0,0 +1,30 @@
+import NavigationBar from './components/navigation-bar/navigation-bar.js'
+
+const navigationItems = [{
+    url: "/hello-world-page",
+    title: 'Hello World',
+},
+    {
+        url: '/kiwi-page',
+        title: 'Kiwi',
+    }]
+
+const navigationBar = new NavigationBar();
+navigationBar.render(navigationItems)
+
+const url = window.location.pathname;
+
+if (url === "/hello-world-page") {
+  import("HelloWorldApp/HelloWorldPage").then((HelloWorldPageModule) => {
+    const HelloWorldPage = HelloWorldPageModule.default;
+    const helloWorldPage = new HelloWorldPage();
+    helloWorldPage.render();
+  });
+} else if (url === "/kiwi-page") {
+  import("KiwiApp/KiwiPage").then((KiwiPageModule) => {
+    const KiwiPage = KiwiPageModule.default;
+    const kiwiPage = new KiwiPage();
+    kiwiPage.render();
+  });
+}
+
---
+++ kiwi/src/kiwi.js
@@ -0,0 +1,13 @@
+// import Heading from "./components/heading/heading.js";
+// import KiwiImage from "./components/kiwi-image/kiwi-image.js";
+
+// const heading = new Heading();
+// heading.render("kiwi");
+
+// const kiwiImage = new KiwiImage();
+// kiwiImage.render();
+
+import KiwiPage from './components/kiwi-page/kiwi-page.js'
+
+const kiwiPage = new KiwiPage();
+kiwiPage.render();
